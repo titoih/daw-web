53c9949c0d13c40a245d8b2d97ecf70eb90761b8
duquediazn <duquediazn@gmail.com>
2024-05-04 12:12:40 +0100

    Haciendo el calendario dinámico con JavaScript

--- script.js
+++ script.js
@@ -16,4 +16,40 @@ function iniciar() {
     });
 }
 
+const currentDate = document.querySelector("#current-date");
+const daysTag = document.querySelector(".days");
 
+//Se guarda en las variables currentYear y currentMonth el mes y año actuales
+let date = new Date();
+let currentYear = date.getFullYear();
+let currentMonth = date.getMonth();
+
+const MONTHS = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre']
+
+const renderCalendar = () => {
+    let lastDateOfMonth = new Date(currentYear, currentMonth + 1, 0).getDate(); //Nos da el último día del mes
+    let liTag = "";
+
+    for (let i = 1; i < lastDateOfMonth; i++) {
+        liTag += `<li>${i}</li>`;
+
+    }
+    currentDate.innerText = `${MONTHS[currentMonth]} ${currentYear}`;
+    daysTag.innerHTML = liTag;
+
+
+}
+
+renderCalendar();
+
+const prevButton = document.querySelector('#prev');
+const nextButton = document.querySelector('#next');
+//Eventos hacer click en botón "prev" o "next":
+prevButton.addEventListener('click', function () {
+    currentMonth = currentMonth - 1;
+    renderCalendar();
+});
+nextButton.addEventListener('click', function () {
+    currentMonth = currentMonth + 1;
+    renderCalendar();
+})
